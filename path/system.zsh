cef9048a63b981b261d2754d95ab407f10a67ade
Jip J. Dekker <jip@dekker.li>
2014-09-10 14:12:19 +0200

    Add default system paths

---
+++ path/system.zsh
@@ -0,0 +1,3 @@
+export PATH="$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:$DOTFILES/bin"
+
+export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
