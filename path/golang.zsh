c78c28c82ca7d1d0b62add516743047af1079237
Jip J. Dekker <jip@dekker.li>
2016-02-10 14:24:03 +0100

    Update to current configuration

--- path/golang.zsh
+++ path/golang.zsh
@@ -1,3 +1,2 @@
-export PATH="$PATH:$HOME/Documents/Go/bin"
-export GOPATH="$HOME/Documents/Go"
-export PATH=$PATH:/usr/local/opt/go/libexec/bin
+export PATH="$PATH:$HOME/.go/bin"
+export GOPATH="$HOME/.go"
