034fce57355ed28c38a500327dbcac9f3c5c547f
Jip J. Dekker <jip@dekker.one>
2017-12-04 15:32:14 +1100

    Add minizinc development functionality

---
+++ autorun/minizinc.zsh
@@ -0,0 +1,29 @@
+function minidev() {
+  # Add the compiled binaries to the path
+  if [ "$1" "==" "release" ]; then
+    export PATH="$MZNSOURCE/build/release:$PATH"
+  else
+    export PATH="$MZNSOURCE/build/debug:$PATH"
+    [ "$1" "!=" "debug" ] && { echo "ʕ•ᴥ•ʔ: I assume you meant \"debug\"!"; }
+  fi
+
+  # Link included libraries
+  ln -sf $MZNSOURCE/share/minizinc/* $MZNLIB
+  export MZN_STDLIB_DIR=$MZNLIB
+}
+
+function contains {
+  string="$1"
+  substring="$2"
+  if test "${string#*$substring}" != "$string"
+  then
+    return 0    # $substring is in $string
+  else
+    return 1    # $substring is not in $string
+  fi
+}
+
+function checkdev() {
+  loc=`which mzn2fzn`
+  contains $loc $MZNSOURCE
+}
---
+++ path/minizinc.zsh
@@ -0,0 +1,6 @@
+# Solvers in the MiniZinc Distribution
+export PATH="/Applications/MiniZincIDE.app/Contents/Resources:$PATH"
+
+export MZNSOURCE="$HOME/Repositories/libmzn"
+
+export MZNLIB="$HOME/Documents/mznlib"
