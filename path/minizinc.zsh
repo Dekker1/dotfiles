d65dc9acd7e98dee58a4a3490a79292745af5329
Jip J. Dekker <jip@dekker.li>
2018-03-27 17:03:41 +1100

    Move minizinc source

--- path/minizinc.zsh
+++ path/minizinc.zsh
@@ -1,6 +1,6 @@
 # Solvers in the MiniZinc Distribution
 export PATH="/Applications/MiniZincIDE.app/Contents/Resources:$PATH"
 
-export MZNSOURCE="$HOME/Repositories/libmzn"
+export MZNSOURCE="$HOME/Repositories/minizinc/libmzn"
 
 export MZNLIB="$HOME/Documents/mznlib"
