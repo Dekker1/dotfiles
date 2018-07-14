abdb4379318e000bc91cac5d2da1dfb7ec2cbffc
Jip J. Dekker <jip@dekker.one>
2018-07-14 18:25:41 +1000

    Update anaconda configuration

--- path/anaconda.zsh
+++ path/anaconda.zsh
@@ -1,1 +1,2 @@
-export PATH=$PATH:/usr/local/anaconda3/bin
+[ -d "/usr/local/anaconda3/bin" ] && export PATH=$PATH:/usr/local/anaconda3/bin
+[ -d "/usr/local/miniconda3/bin" ] && export PATH=$PATH:/usr/local/miniconda3/bin
