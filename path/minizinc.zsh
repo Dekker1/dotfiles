7a70041e6ec87834d3aad1f557561597b6fda5b6
Jip J. Dekker <jip@dekker.one>
2018-07-23 16:53:45 +1000

    Do not automatically push tags

--- path/minizinc.zsh
+++ path/minizinc.zsh
@@ -1,6 +1,6 @@
 # Solvers in the MiniZinc Distribution
 export PATH="/Applications/MiniZincIDE.app/Contents/Resources:$PATH"
 
-export MZNSOURCE="$HOME/Repositories/minizinc/libmzn"
+export MZNSOURCE="$HOME/Repositories/minizinc/minizinc"
 
 export MZNLIB="$HOME/Documents/mznlib"
