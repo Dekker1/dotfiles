773323ae1dc9ee50af984e6e5d4ed56898080957
Jip J. Dekker <jip@dekker.one>
2021-02-16 17:19:03 +1100

    Add MiniZinc lookup areas

--- zsh/autorun/programming.zsh
+++ zsh/autorun/programming.zsh
@@ -6,14 +6,22 @@ export CMAKE_EXPORT_COMPILE_COMMANDS=1
 # CMake use Ninja by default
 export CMAKE_GENERATOR="Ninja Multi-Config"
 
-### Rust ###
-# Add cargo installed binaries to PATH
-if [ -d "$HOME/.cargo/bin" ]; then
-    export PATH=$PATH:"$HOME/.cargo/bin"
+### MiniZinc ###
+
+if [ -d "/opt/homebrew/share/minizinc/solvers" ]; then
+    export MZN_SOLVER_PATH=$MZN_SOLVER_PATH:"/opt/homebrew/share/minizinc/solvers"
 fi
+
 
 ### Python ###
 # Add pipx installed binaries to PATH
 if [ -d "$HOME/.local/bin" ]; then
     export PATH=$PATH:"$HOME/.local/bin"
 fi
+
+### Rust ###
+# Add cargo installed binaries to PATH
+if [ -d "$HOME/.cargo/bin" ]; then
+    export PATH=$PATH:"$HOME/.cargo/bin"
+fi
+
