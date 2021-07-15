4f096d11ed88690496f1c667a68a5f9bc6cc2f12
Jip J. Dekker <jip@dekker.one>
2021-07-15 10:53:51 +1000

    Switch to main branch of indentlines

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -16,7 +16,7 @@ require('packer').startup(function()
 	-- Appearance
 	use 'folke/tokyonight.nvim' -- Theme
 	use 'hoob3rt/lualine.nvim' -- Status line
-	use { 'lukas-reineke/indent-blankline.nvim', branch="lua" } -- Add indentation guides even on blank lines
+	use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
 	use 'folke/which-key.nvim' -- Show key mappings
 	-- Component: Language Server Protocol
 	use 'neovim/nvim-lspconfig' -- Configuration of LSP
