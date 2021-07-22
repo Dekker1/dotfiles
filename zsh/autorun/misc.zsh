21631bd6fe2644a824ced4142a2e019f4a6178e6
Jip J. Dekker <jip@dekker.one>
2021-07-23 09:57:41 +1000

    Initial solution to change kitty colour scheme without changing config

--- zsh/autorun/misc.zsh
+++ zsh/autorun/misc.zsh
@@ -3,5 +3,9 @@
 # Reload zsh configuration
 alias reload='. ~/.zshrc'
 
+# Change Kitty themes
+alias darkkitty='kitty @ set-colors --all ~/.local/share/nvim/site/pack/packer/start/tokyonight.nvim/extras/kitty_tokyonight_storm.conf'
+alias lightkitty='kitty @ set-colors --all .config/kitty/themes/papercolor-light.conf'
+
 # MiniZinc
 export BIGBAD="compute.optimisation-2020.cloud.edu.au"
