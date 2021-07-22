85faadfcb13ab459720398b5afd0cdbb5cac03b8
Jip J. Dekker <jip@dekker.one>
2021-07-22 10:32:50 +1000

    Fix the naming of the python lsp configuration

--- config/nvim/lua/lang/python.lua
+++ config/nvim/lua/lang/python.lua
@@ -1,7 +1,7 @@
 local lsp_settings = require('comp.lsp')
 local nvim_lsp = require('lspconfig')
 
-nvim_lsp.pyls.setup {
+nvim_lsp.pylsp.setup {
 	on_attach = lsp_settings.on_attach,
 	capabilities = lsp_settings.capabilites,
 }
